ed9722ca5d98ee2a68bf9cc381ee691d4eaab8a7
Mahmoud <merzebany@gmail.com>
2025-12-01 01:01:44 +0200

    p_04

--- My_App/static/js/main.js
+++ My_App/static/js/main.js
@@ -1,15 +1,14 @@
+const toggle = document.querySelector('#toggle'); 
+const password_v = document.querySelector('#password');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const toggle = document.getElementById("toggle");
-    const password = document.getElementById("password"); // Make sure this exists too
-    
-    toggle.addEventListener("click", () => {
-        if (password.type == "password") {
-            password.type = "text";
-            toggle.classList.replace('bi-eye', 'bi-eye-slash');
-        } else {
-            password.type = "password";
-            toggle.classList.replace('bi-eye-slash', 'bi-eye');
-        }
-    });
-});
+  toggle.addEventListener("click", () => {
+    if (password_v.type == "password") {
+      password_v.type = "text";
+      toggle.classList.replace('bi-eye', 'bi-eye-slash');
+    }
+    else {
+      password_v.type = "password";
+      toggle.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+  });  
+
