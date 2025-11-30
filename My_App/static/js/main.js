97d2e5ae6024be39446d25f7481b19c87d070b46
Mahmoud <merzebany@gmail.com>
2025-11-30 23:14:27 +0200

    p_03

--- My_App/static/js/main.js
+++ My_App/static/js/main.js
@@ -1,16 +1,15 @@
 
-const password = document.getElementById("password")
-
-const toggle = document.getElementById("toggle")
-
-toggle.addEventListener("click", () => {
-  if (password.type == "password") {
-    password.type = "text";
-    toggle.classList.replace('bi-eye', 'bi-eye-slash');
-  }
-  else {
-     password.type = "password";
-     toggle.classList.replace('bi-eye-slash', 'bi-eye' );
-  }
-      
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.getElementById("toggle");
+    const password = document.getElementById("password"); // Make sure this exists too
+    
+    toggle.addEventListener("click", () => {
+        if (password.type == "password") {
+            password.type = "text";
+            toggle.classList.replace('bi-eye', 'bi-eye-slash');
+        } else {
+            password.type = "password";
+            toggle.classList.replace('bi-eye-slash', 'bi-eye');
+        }
+    });
+});
